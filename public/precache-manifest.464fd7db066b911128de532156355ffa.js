0c8cf17db35ec698bf096392afcd3b3925b6d2ed
Jake Overall <joverall22@gmail.com>
2019-09-05 20:52:15 -0600

    updated public

---
+++ public/precache-manifest.464fd7db066b911128de532156355ffa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cd8787a50fae9391a57",
+    "url": "/css/app.889365d7.css"
+  },
+  {
+    "revision": "4cd8787a50fae9391a57",
+    "url": "/js/app.02e39b81.js"
+  },
+  {
+    "revision": "e1236b7e5ebb5fdd3dd4",
+    "url": "/js/chunk-vendors.eebc5769.js"
+  },
+  {
+    "revision": "79285fa8d0cbbde32fddd5ac579320ef",
+    "url": "/img/banner.79285fa8.png"
+  },
+  {
+    "revision": "99e135dd034b7550bc0fc662af09e3d2",
+    "url": "/img/blogger-logo.99e135dd.png"
+  },
+  {
+    "revision": "ec09e6ac9a206fc0b9f88bf371ad2a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d3c1f4a097eaad301a1a795a555438a",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "951a67a8a6d9e122fd4257c4b90a0fab",
+    "url": "/favicon-blogger.ico"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
